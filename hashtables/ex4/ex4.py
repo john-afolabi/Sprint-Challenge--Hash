565ba0c66978e78460993951d00be2f00592cc50
John Afolabi <johnafolabi.da@gmail.com>
2020-05-08 11:01:54 +0100

    Complete exvercise 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,17 @@
 def has_negatives(a):
-
     """
     YOUR CODE HERE
     """
 
+    hashtable = {}
+    result = []
+    for i, num in enumerate(a):
+        hashtable[abs(num)] = i
+        # print(hashtable)
+        if num not in hashtable:
+            result.append(abs(num))
     return result
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1,-2,1,2,3,4,-4]))
+    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
